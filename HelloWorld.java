54b95b63afebfbbe4c830a9d7f7eb2a0893399cd
prasanna2294 <123261210+prasanna2294@users.noreply.github.com>
2023-10-08 20:35:09 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
-public static void main(String[] args){
+  public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 
-system.out.pintln("Hello java");
-}
 }
